bf48ba47b1616960ffad20adf69f3ada80139ae3
Nuel Agyei <nuelagyei992@gmail.com>
2023-09-26 10:04:32 +0000

    avatar fix

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -12,7 +12,12 @@ class AvatarController extends Controller
             'avatar' => ['required','image'],
         ]);
         $path = $request->file('avatar')->store('avatars', 'public');
+
+        if($request->user()->avatar){
+
+        }
         auth()->user()->update(['avatar'=> $path]);
+        // dd(auth()->user()->avatar);
         return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
     }
 }
