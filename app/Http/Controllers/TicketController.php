1d09ea9c8e7c4a4018eb050e25ee39ef40e0f367
Nuel Agyei <nuelagyei992@gmail.com>
2023-09-29 09:06:09 +0000

    Ticket deletion

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -87,6 +87,7 @@ class TicketController extends Controller
      */
     public function destroy(Ticket $ticket)
     {
-        //
+        $ticket->delete();
+        return response()->redirect();
     }
 }
